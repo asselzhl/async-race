262b65e50ecc47b9946dc8e95a0f0403a0ec7e6c
asselzhl <asselzhly@gmail.com>
2024-06-14 15:56:05 +0500

    feat: implement Garage page

--- src/pages/Garage.tsx
+++ src/pages/Garage.tsx
@@ -1,5 +1,15 @@
+import ControlPanel from '../components/ControlPanel/ControlPanel';
+import Track from '../components/Track/Track';
+import GarageStatus from '../components/GarageStatus/GarageStatus';
+
 function Garage() {
-  return <div>Garage</div>;
+  return (
+    <>
+      <ControlPanel />
+      <Track />
+      <GarageStatus />
+    </>
+  );
 }
 
 export default Garage;

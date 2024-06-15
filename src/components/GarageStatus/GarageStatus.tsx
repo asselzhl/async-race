1e8d759fe8c795700fc8b7ea32f0122376c91a00
asselzhl <asselzhly@gmail.com>
2024-06-15 10:43:23 +0500

    feat: update GarageStatus to show the cars number in garage

--- src/components/GarageStatus/GarageStatus.tsx
+++ src/components/GarageStatus/GarageStatus.tsx
@@ -1,12 +1,16 @@
 import { Pagination } from '@mui/material';
 
+import { useSelector } from 'react-redux';
 import styles from './GarageStatus.module.css';
+import { getCarsList } from '../../store/selectors';
 
 function GarageStatus() {
+  const carsList = useSelector(getCarsList);
+
   return (
     <div className={styles.wrapper}>
       <p>
-        Garage <span>(0)</span>
+        Garage <span>({carsList.length})</span>
       </p>
       <Pagination count={10} shape="rounded" />
     </div>

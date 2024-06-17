2f5c24ff4bda1694e13dedaa5d7078280fb2b608
asselzhl <asselzhly@gmail.com>
2024-06-17 14:28:02 +0500

    feat: implement pages and carForm slices

--- src/store/pages/pagesSlice.ts
+++ src/store/pages/pagesSlice.ts
@@ -1,23 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 interface PagesState {
-    currentPage: number;
+  currentPage: number;
 }
 
 const initialState: PagesState = {
-    currentPage: 1
-}
+  currentPage: 1,
+};
 
 export const pagesSlice = createSlice({
-    name: 'pages',
-    initialState,
-    reducers: {
-        setCurrentPage: (state, action) => {
-            state.currentPage = action.payload;
-            return state;
-        }
-    }
-})
+  name: 'pages',
+  initialState,
+  reducers: {
+    setCurrentPage: (state, action) => {
+      state.currentPage = action.payload;
+      return state;
+    },
+  },
+});
 
-export const { setCurrentPage } = pagesSlice.actions
-export const pagesReducer = pagesSlice.reducer
+export const { setCurrentPage } = pagesSlice.actions;
+export const pagesReducer = pagesSlice.reducer;

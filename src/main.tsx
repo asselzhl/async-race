a369e2a712af5a5dd22af91acd7f8d83176d84e4
asselzhl <asselzhly@gmail.com>
2024-06-17 14:24:41 +0500

    fear: remove redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './store/store';
+import { store } from './store/store';
 
 import App from './App';
 import './index.css';
@@ -12,9 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
+        <App />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,48 +1,34 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { carReducer } from './car/carSlice';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
+import { carReducer } from './car/carSlice';
+import { carFormReducer } from './carForm/carFormSlice';
+import { pagesReducer } from './pages/pagesSlice';
 
 export const rootReducer = combineReducers({
   cars: carReducer,
+  carForm: carFormReducer,
+  pages: pagesReducer,
 });
 
-export const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [
-          FLUSH,
-          REHYDRATE,
-          PAUSE,
-          PERSIST,
-          PURGE,
-          REGISTER,
           'getCars/fulfilled',
+          'getCars/rejected',
+          'createCar/fulfilled',
+          'createCar/rejected',
+          'deleteCar/fulfilled',
+          'deleteCar/rejected',
+          'updateCar/fulfilled',
+          'updateCar/rejected',
         ],
       },
     }),
 });
-
-export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;

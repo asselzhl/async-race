00e3a4a33ef1f869d84419d974aa60c51ccbe44d
asselzhl <asselzhly@gmail.com>
2024-06-18 13:25:07 +0500

    feat: refactor  component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import { Container } from '@mui/material';
-import Garage from './pages/Garage';
-import Winners from './pages/Winners';
-import routePaths from './helpers/routePaths';
-import Header from './components/Header/Header';
+import { Garage } from './pages/Garage';
+import { Winners } from './pages/Winners';
+import { routePaths } from './helpers/routePaths';
+import { Header } from './components/Header/Header';
 
 function App() {
-  // TODO: routesConfig type
-  const routesConfig = [
-    {
-      path: routePaths.index,
-      element: <Garage />,
-    },
-    {
-      path: routePaths.winners,
-      element: <Winners />,
-    },
-  ];
-
   return (
     <Container>
       <Header />
       <Routes>
-        {routesConfig.map((route) => {
-          return (
-            <Route key={route.path} path={route.path} element={route.element} />
-          );
-        })}
+        <Route path={routePaths.index} element={<Garage />} />
+        <Route path={routePaths.winners} element={<Winners />} />
       </Routes>
     </Container>
   );

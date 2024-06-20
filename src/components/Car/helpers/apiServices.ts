734e0be3bf335b226e5529a1638dbbfa43109bef
asselzhl <asselzhly@gmail.com>
2024-06-20 09:48:00 +0500

    refactor: change function name

--- src/components/Car/helpers/apiServices.ts
+++ src/components/Car/helpers/apiServices.ts
@@ -1,13 +1,23 @@
 import { agent } from '../../../store/car/carThunk';
 
-export const makePatchRequest = (ID: number, status: string) => {
-  return agent.patch(`/engine?id=${ID}&status=${status}`);
+type EngineStatus = 'started' | 'stopped' | 'drive';
+interface RequestParams {
+  carID: number;
+  status: EngineStatus;
+}
+// eslint-disable-next-line
+export const changeEngineStatus = (params: RequestParams) => {
+  try {
+    return agent.patch(`/engine?id=${params.carID}&status=${params.status}`);
+  } catch (error) {
+    // eslint-disable-next-line
+    console.error(error);
+  }
 };
-
 // eslint-disable-next-line
 export const driveCar = async (ID: number) => {
   try {
-    const response = await makePatchRequest(ID, 'drive');
+    const response = await changeEngineStatus({ carID: ID, status: 'drive' });
     return response;
   } catch (error) {
     // eslint-disable-next-line
